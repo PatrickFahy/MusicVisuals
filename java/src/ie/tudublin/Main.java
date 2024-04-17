8b4c804f980ee462e9657b8fc0d9856c2138f9ce
CS++ <C22393366@mytudublin.ie>
2024-04-17 15:18:48 +0100

    Accepting File

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -13,6 +13,7 @@ public class Main {
     public void startUI() {
         String[] a = { "MAIN" };
         processing.core.PApplet.runSketch(a, new Assignment_Test());
+
     }
 
     public static void main(String[] args) {
