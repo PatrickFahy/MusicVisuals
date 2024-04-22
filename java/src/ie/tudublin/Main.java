4aa1f0292e1c2a13e6154d365a1a67db3b6ee99e
CS++ <C22393366@mytudublin.ie>
2024-04-22 21:20:06 +0100

    test for sam 2

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -8,16 +8,12 @@ import example.MyVisual;
 import example.jeff;
 import example.RotatingAudioBands;
 import example.RotatingBands;
-import example.Cormac;
+import example.cormac;
 public class Main {
 
     public void startUI() {
         String[] a = { "MAIN" };
-<<<<<<< HEAD
-        processing.core.PApplet.runSketch(a, new Cormac());
-=======
-        processing.core.PApplet.runSketch(a, new jeff());
->>>>>>> 18d223b1286ce0222f29ce909b33f7b23e76f3f4
+        processing.core.PApplet.runSketch(a, new cormac());
     }
 
     public static void main(String[] args) {
