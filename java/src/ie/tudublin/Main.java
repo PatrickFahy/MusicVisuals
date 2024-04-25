511503235a0a5f6d30e0fb94708b82b3575a44b2
PatrickFahy <C22394713@mytudublin.ie>
2024-04-25 16:33:40 +0100

    committing to pull

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public void startUI() {
         String[] a = { "MAIN" };
-        processing.core.PApplet.runSketch(a, new patrick());
+        processing.core.PApplet.runSketch(a, new ProjectVisual());
     }
 
     public static void main(String[] args) {
