c45fc3b291789ebae8fdbbbc1c5b666ec3b605cd
CoHolohan <cormacholohanc2@gmail.com>
2024-04-25 18:08:52 +0100

    Cube visual in switch working

--- java/src/ie/tudublin/Cormac_cubes.java
+++ java/src/ie/tudublin/Cormac_cubes.java
@@ -13,14 +13,14 @@ public class Cormac_cubes extends poly {
     AudioInput ai;
     AudioBuffer ab;
     float rotationSpeed = 0.0001f; // Speed of rotation
-    float sizeMultiplier = 200; // Increase the size multiplier for a larger base size
+    float sizeMultiplier = 150; // Increase the size multiplier for a larger base size
 
-    int numCubes = 500; // Number of cubes (particles)
-    float[] cubeXPositions; // X positions of cubes
-    float[] cubeYPositions; // Y positions of cubes
-    float[] cubeZPositions; // Z positions of cubes
-    float[] cubeVelocities; // Velocities of cubes
-    int[] cubeColors; // Colors of cubes
+    // int numCubes = 500; // Number of cubes (particles)
+    // float[] cubeXPositions; // X positions of cubes
+    // float[] cubeYPositions; // Y positions of cubes
+    // float[] cubeZPositions; // Z positions of cubes
+    // float[] cubeVelocities; // Velocities of cubes
+    // int[] cubeColors; // Colors of cubes
 
     public Cormac_cubes(ProjectVisual v){
         super(v);
@@ -32,63 +32,50 @@ public class Cormac_cubes extends poly {
         Comac_cubes_draw();
     }
 
-    public void settings() {
-        v.size(v.width, v.height); // Use P3D renderer for 3D
-    }
-
-    public void setup() {
-        minim = new Minim(this);
-        ap = minim.loadFile("The Chemical Brothers - We've Got To Try.mp3");
-        ap.play();
-        ab = ap.mix;
-
-        cubeXPositions = new float[numCubes]; // Initialize arrays
-        cubeYPositions = new float[numCubes];
-        cubeZPositions = new float[numCubes];
-        cubeVelocities = new float[numCubes];
-        cubeColors = new int[numCubes];
-
-        // Randomize initial positions, velocities, and colors
-        for (int i = 0; i < numCubes; i++) {
-            cubeXPositions[i] = v.random((v.width *-1)/ 2, v.width / 2);
-            cubeYPositions[i] = v.random((v.height * -1)/ 2, v.height / 2);
-            cubeZPositions[i] = v.random(-500, -1000);
-            cubeVelocities[i] = v.random(1, 5);
-            cubeColors[i] = v.color(v.random(255), v.random(255), v.random(255)); // Random color
-        }
-    }
 
     public void Comac_cubes_draw() {
         v.background(0);
         v.lights(); // Enable lighting
-
+    
         // Calculate amplitude from audio buffer
-        float amplitude = ab.level() * sizeMultiplier;
-
+        float amplitude = v.ab.level() * sizeMultiplier;
+    
         // Translate to the center of the window
         v.translate(v.width / 2, v.height / 2, 0);
-
+    
         // Update and draw cubes
-        for (int i = 0; i < numCubes; i++) {
-            cubeZPositions[i] += cubeVelocities[i]; // Move cube towards the camera
-            drawCube(amplitude, cubeXPositions[i], cubeYPositions[i], cubeZPositions[i], cubeColors[i]); // Draw cube at updated position
-
+        for (int i = 0; i < v.numCubes; i++) {
+            v.cubeZPositions[i] -= v.cubeVelocities[i]; // Move cube towards the camera
+    
+            drawCube(amplitude, v.cubeXPositions[i], v.cubeYPositions[i], v.cubeZPositions[i], v.cubeColors[i]); // Draw cube at updated position
+    
             // Reset cube position if it goes beyond the camera
-            if (cubeZPositions[i] > 0) {
-                cubeZPositions[i] = v.random(-500, -1000);
-                cubeXPositions[i] = v.random((v.width *-1) / 2, v.width / 2);
-                cubeYPositions[i] = v.random((v.height * -1) / 2, v.height / 2);
-                cubeVelocities[i] = v.random(1, 5);
-                cubeColors[i] = v.color(v.random(255), v.random(255), v.random(255)); // Random color
+            if (v.cubeZPositions[i] > 200) {
+                v.cubeZPositions[i] = v.random(-500, -3000);
+                v.cubeXPositions[i] = v.random((v.width *-1) / 2, v.width / 2);
+                v.cubeYPositions[i] = v.random((v.height * -1) / 2, v.height / 2);
+                v.cubeVelocities[i] = v.random(0.1f, 1f);
+                v.cubeColors[i] = v.color(v.random(255), v.random(255), v.random(255)); // Random color
             }
         }
     }
-
+    
     void drawCube(float size, float x, float y, float z, int cubeColor) {
+
+        for (int i = 0; i < v.numCubes; i++) {
+            v.cubeXPositions[i] = v.random((v.width *-1), v.width);
+            v.cubeYPositions[i] = v.random((v.height * -1), v.height);
+            v.cubeZPositions[i] = v.random(-800, -1500);
+            v.cubeVelocities[i] = v.random(1, 5);
+            v.cubeColors[i] = v.color(v.random(255), v.random(255), v.random(255)); // Random color
+        }
+
+        
+        
         // Draw cube with size determined by audio amplitude
         v.pushMatrix(); // Save current transformation matrix
         v.translate(x, y, z); // Translate to position
-        v.stroke(cubeColor); // White outline
+        v.stroke(cubeColor); // Colour outline for the cubes
         v.fill(cubeColor); // Random fill color
         v.box(size); // Draw cube
         v.popMatrix(); // Restore previous transformation matrix
--- java/src/ie/tudublin/Visual.java
+++ java/src/ie/tudublin/Visual.java
@@ -3,13 +3,6 @@ package ie.tudublin;
 import processing.core.*;
 import ddf.minim.*;
 import ddf.minim.analysis.*;
-import ddf.minim.analysis.FFT;
-import ddf.minim.AudioBuffer;
-import ddf.minim.AudioInput;
-import ddf.minim.AudioPlayer;
-import ddf.minim.Minim;
-import processing.core.PApplet;
-import processing.core.PVector;
 
 
 
@@ -62,8 +55,18 @@ public abstract class Visual extends PApplet
 
 
 // Cormac Variables
+	//Cormac_lightning Variables
+	//Cormac_lightning.rain[] rains = new Cormac_lightning.rain[125]; // Array to hold rains
+	
 
+	// Cormac_cubes Variables
+	int numCubes = 500;
 
+	float[] cubeXPositions = new float[numCubes]; // X positions of cubes
+    float[] cubeYPositions = new float[numCubes]; // Y positions of cubes
+    float[] cubeZPositions = new float[numCubes]; // Z positions of cubes
+    float[] cubeVelocities = new float[numCubes]; // Velocities of cubes
+    int[] cubeColors = new int[numCubes];; // Colors of cubes
 
 //End Cormac Variables
 
